80a895cecec3fbd24a002678a7bdd97f9c378ece
Kirill Shchepelin <kirill.shchepelin.72@gmail.com>
2023-01-27 03:37:41 +0600

    Добавьте файлы проекта.

---
+++ Lab1/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Lab1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число a:");
+            double a = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите число b:");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine($"Вы ввели число a - '{a}', число b - '{b}'");
+
+            Console.WriteLine($"a + b = {a + b}");
+            Console.WriteLine($"a - b = {a - b}");
+            Console.WriteLine($"a * b = {a * b}");
+            Console.WriteLine($"a / b = {a / b}");
+
+            Console.WriteLine("Нажмите любую клавишу для завершения");
+            Console.ReadKey();
+        }
+    }
+}
