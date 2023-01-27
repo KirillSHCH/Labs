5449d81eeb7748417c20e94d579d1b400916dd21
Kirill Shchepelin <kirill.shchepelin.72@gmail.com>
2023-01-27 23:19:50 +0600

    init lab3

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -4,21 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Введите число a:");
-            double a = Convert.ToDouble(Console.ReadLine());
-
-            Console.WriteLine("Введите число b:");
-            double b = Convert.ToDouble(Console.ReadLine());
-
-            Console.WriteLine($"Вы ввели число a - '{a}', число b - '{b}'");
-
-            Console.WriteLine($"a + b = {a + b}");
-            Console.WriteLine($"a - b = {a - b}");
-            Console.WriteLine($"a * b = {a * b}");
-            Console.WriteLine($"a / b = {a / b}");
-
-            Console.WriteLine("Нажмите любую клавишу для завершения");
-            Console.ReadKey();
+            // TODO: ALL
         }
     }
 }
